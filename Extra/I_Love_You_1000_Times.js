ff1775b6e1d2c35d34ee317df94cce4f81467d76
Pablo Vargas <Rojaspablotomas@gmail.com>
2025-12-01 14:02:36 -0500

    Create new carpet Extra

---
+++ Extra/I_Love_You_1000_Times.js
@@ -0,0 +1,8 @@
+// Make a soluction to print 1000 times - I love You in Spanish
+
+for (let i = 1; i <= 1000; i++) {
+    console.log("Te amo ❤️");
+    let porcentaje = (i / 1000) * 100;
+    console.log(`${i}/1000  -  ${porcentaje.toFixed(2)}%`);
+}
+
